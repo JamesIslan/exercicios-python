c076f5b7871c00cb57a022123869ca8e486a0165
Joamerson Islan <joamersonislan38@gmail.com>
2022-04-08 18:02:36 -0300

    Tratamento de erro incluso

--- 7.py
+++ 7.py
@@ -1,4 +1,19 @@
-nota1 = float(input("Digite aqui a primeira nota do aluno: "))
-nota2 = float(input("Digite aqui a segunda nota do aluno: "))
-media_notas = float((nota1 + nota2) / 2)
-print(f'A média aritmética simples das duas notas é de {media_notas} pontos')
+# Tratamento de erro incluso
+nota1, nota2 = float(), float()
+continua = str()
+while continua != 'n':
+    try:
+        nota1 = float(input("Digite aqui a primeira nota do aluno: "))
+        nota2 = float(input("Digite aqui a segunda nota do aluno: "))
+    except ValueError:
+        print(f'Digite apenas números inteiros ou de ponto flutuante!')
+        continue
+    media_notas = float((nota1 + nota2) / 2)
+    print(f'A média aritmética simples das duas notas é de {media_notas:.2f} pontos')
+    while True:
+        continua = str(input('Deseja fazer o cálculo de outra nota? (S/N): ')).lower()
+        if continua not in 'sn':
+            print('Digite uma opção válida!')
+            continue
+        else:
+            break
