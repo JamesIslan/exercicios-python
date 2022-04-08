ef8f2bb08710f708b7ae12460b193ba9bb50e052
Joamerson Islan <joamersonislan38@gmail.com>
2022-04-08 17:40:43 -0300

    Tratamento de erro incluso

--- 6.py
+++ 6.py
@@ -1,5 +1,13 @@
-numero = int(input('Digite aqui um número inteiro: '))
-dobro = numero * 2
-triplo = numero * 3
-raiz_quadrada = int(numero ** (1/2))
-print(f'O dobro de {numero} é {dobro}, o triplo de {numero} é {triplo} e a raíz quadrada de {numero} é {raiz_quadrada}')
+# Tratamento de erro incluso
+from math import sqrt
+numero = int()
+while True:
+    try:
+        numero = int(input('Digite aqui um número inteiro: '))
+    except ValueError:
+        print('Digite apenas números inteiros!')
+        continue
+    dobro = numero * 2
+    triplo = numero * 3
+    raiz_quadrada = sqrt(numero)
+    print(f'O dobro de {numero} é {dobro}, o triplo de {numero} é {triplo} e a raíz quadrada de {numero} é {raiz_quadrada}')
