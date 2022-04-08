00a2a7846c54c07901bb713915f1cf59cae4dbba
Joamerson Islan <joamersonislan38@gmail.com>
2022-04-08 17:33:10 -0300

    Tratamento de erro incluso

--- 5.py
+++ 5.py
@@ -1,4 +1,10 @@
-numero = int(input("Digite aqui um número inteiro: "))
-antecessor = numero - 1
-sucessor = numero + 1
-print(f'O antecessor de {numero} é {antecessor}, e o sucessor de {numero} é {sucessor}')
+# Tratamento de erro incluso
+while True:
+    try:
+        numero = int(input("Digite aqui um número inteiro: "))
+    except ValueError:
+        print(f'Digite apenas números inteiros!')
+        continue
+    antecessor = numero - 1
+    sucessor = numero + 1
+    print(f'O antecessor de {numero} é {antecessor}, e o sucessor de {numero} é {sucessor}')
