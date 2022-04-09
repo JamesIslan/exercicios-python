ce62375a5c47d232591bd5546933c33907990b96
Joamerson Islan <joamersonislan38@gmail.com>
2022-04-08 21:09:05 -0300

    Tratamento de erro incluso

--- 8.py
+++ 8.py
@@ -1,4 +1,18 @@
-valor_metros = float(input('Digite aqui o valor em metros: '))
-conversao_centimetros = valor_metros * 100
-conversao_milimetros = conversao_centimetros * 10
-print(f'{valor_metros} equivale a {conversao_centimetros} centímetros ou {conversao_milimetros} milímetros')
+# Tratamento de erro incluso
+continua = str()
+while continua != 'n':
+    try:
+        valor_metros = float(input('Digite aqui o valor em metros: '))
+    except ValueError:
+        print(f'Digite apenas números inteiros ou de ponto flutuante!')
+        continue
+    conversao_centimetros = valor_metros * 100
+    conversao_milimetros = conversao_centimetros * 10
+    print(f'{valor_metros} equivale a {conversao_centimetros:.2f} centímetros ou {conversao_milimetros:.3f} milímetros')
+    while True:
+        continua = str(input(f'Deseja fazer outra conversão? (S/N)')).lower()
+        if continua not in 'sn':
+            print('Digite uma opção válida!')
+            continue
+        else:
+            break
