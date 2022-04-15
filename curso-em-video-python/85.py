7c0defc9bbc46b5f96c0b6446bdaf9272a75a196
Joamerson Islan <joamersonislan38@gmail.com>
2022-04-15 09:56:29 -0300

    Tratamento de erro incluso

---
+++ curso-em-video-python/85.py
@@ -0,0 +1,20 @@
+#Tratamento de erro incluso
+lista = list()
+
+for i in range(7):
+    while True:
+        try:
+            valor = int(input('Digite aqui um número inteiro: '))
+            lista += [valor]
+            break
+        except ValueError:
+            print('Digite um valor válido!')
+            continue
+
+pares = list(sorted(filter(lambda x: x % 2 == 0, lista)))
+impares = list(sorted(filter(lambda x: x % 2 == 1, lista)))
+
+print('-='*35)
+print(f'Os números pares digitados foram: {str(pares)[1:-2]}')
+print(f'Os números ímpares digitados foram: {str(impares)[1:-2]}', sep=', ')
+print('-='*35)
